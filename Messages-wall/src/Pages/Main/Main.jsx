2f52c210860f1b3df20db6d964421796320d2dd0
Togu <timeofgetup@gmail.com>
2023-05-26 00:35:56 -0500

    add item message

---
+++ Messages-wall/src/Pages/Main/Main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import MessajeItem from './MessajeItem'
+
+const Main = () => {
+  return (
+    <>
+    <MessajeItem></MessajeItem>
+    </>
+    
+  )
+}
+
+export default Main
---
+++ Messages-wall/src/Pages/Main/MessajeItem.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const MessajeItem = () => {
+    return (
+        <>
+            <div className="ag-format-container">
+                <div className="ag-courses_box">
+                    <div className="ag-courses_item">
+                        <a href="#" className="ag-courses-item_link">
+                            <div className="ag-courses-item_bg"></div>
+
+                            <div className="ag-courses-item_title">
+                            Cuando te encuentres con tu Dios, dile que me deje en paz.
+                            </div>
+
+                            <div className="ag-courses-item_date-box">
+                                Start:
+                                <span className="ag-courses-item_date">
+                                    04.11.2022
+                                </span>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+            </div>
+
+        </>
+    )
+}
+
+export default MessajeItem
